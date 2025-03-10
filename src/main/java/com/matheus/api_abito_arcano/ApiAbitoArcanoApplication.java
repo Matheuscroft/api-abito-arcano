1b848d240f74557ebcb5ff82d414d82a38276113
Matheus OS <matheus.sdoliveira@gmail.com>
2025-03-10 18:06:26 -0300

    Initial commit

---
+++ src/main/java/com/matheus/api_abito_arcano/ApiAbitoArcanoApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.api_abito_arcano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAbitoArcanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAbitoArcanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/api_abito_arcano/ApiAbitoArcanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.api_abito_arcano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAbitoArcanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
