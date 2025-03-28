a79f8149a810494956dfe9bcff20d32946255400
Matheus OS <matheus.sdoliveira@gmail.com>
2025-03-28 13:49:42 -0300

    inclusão do front vercel no cors.

--- src/main/java/com/matheus/api_abito_arcano/config/CorsConfig.java
+++ src/main/java/com/matheus/api_abito_arcano/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://app-habito-list.vercel.app/")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
