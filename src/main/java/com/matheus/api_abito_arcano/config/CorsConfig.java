4fea9ced63f3658ab4ae430af6d709d82b78d447
Matheus OS <matheus.sdoliveira@gmail.com>
2025-03-28 13:32:19 -0300

    alteração no cors

--- src/main/java/com/matheus/api_abito_arcano/config/CorsConfig.java
+++ src/main/java/com/matheus/api_abito_arcano/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://api-abito-arcano-prd.up.railway.app")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
